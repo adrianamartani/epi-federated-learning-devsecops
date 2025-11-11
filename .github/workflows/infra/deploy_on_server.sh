758f0188a5a11f76732e6cc525d346c370b57f51
Adriana Monteiro Martani <80915974+adrianamartani@users.noreply.github.com>
2025-11-11 14:03:55 -0300

    Update deploy_on_server.sh

--- .github/workflows/infra/deploy_on_server.sh
+++ .github/workflows/infra/deploy_on_server.sh
@@ -2,7 +2,7 @@ set -e
 # repo no server
 WORKDIR=/srv/tcc-epi-federated
 if [ ! -d "$WORKDIR" ]; then
-  git clone https://github.com/YOUR_GITHUB_USER/YOUR_REPO.git $WORKDIR
+  git clone https://github.com/adrianamartani/epi-federated-learning-devsecops.git $WORKDIR
 else
   cd $WORKDIR
   git pull
